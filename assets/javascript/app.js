688975d6e555349070b575619fe0e630e97dd0ed
jmj928 <jmj928@vt.edu>
2018-09-09 23:37:38 -0400

    final commit v4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,6 +27,9 @@ $(document).ready(function () {
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
           selectedCartoon + "&api_key=r9MUU05qDP5SWkY29SY6fD1CXN4SUspx&limit=10";
         console.log("Query " + queryURL);
+        var queryURL2 = "https://www.omdbapi.com/?t=" + selectedCartoon + "&y=&plot=short&apikey=96837c43";
+        var gifDiv = $("<div class='item'>");
+        console.log("Query " + queryURL2);
         $.ajax({
           url: queryURL,
           method: "GET"
@@ -36,23 +39,57 @@ $(document).ready(function () {
             console.log(results);
   
             for (var i = 0; i < results.length; i++) {
-              var gifDiv = $("<div class='item'>");
+              
   
               var rating = results[i].rating;
+              var title = results[i].title;
+              var imdbRating = $("<p>");
+              var totalSeasons;
   
-              var p = $("<p>").text("Rating: " + rating);
+              var p = $("<p>").text("Rating: " + rating );
   
               var cartoonImage = $("<img>");
               cartoonImage.attr("src", results[i].images.fixed_height_still.url);
+              cartoonImage.attr("alt", title);
             
-              gifDiv.prepend(p);
-              gifDiv.prepend(cartoonImage);
+
+
+              $.ajax({
+                url: queryURL2,
+                method: "GET"
+              })
+                .then(function(response2) {
+
+                
+                  var results2 = response2;
+                  console.log("IMDB Resutls" + results2.imdbRating);
+
+                  if(typeof results2.totalSeasons === 'undefined'){
+                    imdbRating.text("IMDB Rating : " + results2.imdbRating + " Total Seasons : Not Listed " );
+
+                  }
+                  else{
+        
+                    imdbRating.text("IMDB Rating : " + results2.imdbRating + " Total Seasons : " + results2.totalSeasons);
+                  }                    
+                    
+                    
+        
+                    
+                  
+                });
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(cartoonImage);
+                gifDiv.prepend(imdbRating);
               
-              
+                $("#gifs-appear-here").prepend(gifDiv);
   
-              $("#gifs-appear-here").prepend(gifDiv);
+            
             }
           });
+
+
       }
 
 
